8825cda800b1eff79103d7adadbb1e1151958947
Aayush Bajracharya <atrexia@gmail.com>
2018-05-08 09:31:39 +0545

    Initial Commit

---
+++ KindleLBTA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KindleLBTA
+//
+//  Created by Aayush Bajracharya on 5/8/18.
+//  Copyright © 2018 Aayush Bajracharya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
